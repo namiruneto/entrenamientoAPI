b3b1cdef3445d18b699e3c8fc404e9a9b8e67739
Cesar Fernando Carreño Peña <sql.programador16@opasascomco.onmicrosoft.com>
2025-01-27 10:09:36 -0500

    chose(RespuestaPreguntas): se va manejar la respuesta de la IA y la pregunta que se genero para procesar

---
+++ entrenamientoAPI/Entities/RespuestaPreguntas.cs
@@ -0,0 +1,8 @@
+﻿namespace entrenamientoAPI.Entities
+{
+    public class RespuestaPreguntas
+    {
+        public string Pregunta { get; set; }
+        public RespuestaDeApi Respuesta { get; set; }
+    }
+}
