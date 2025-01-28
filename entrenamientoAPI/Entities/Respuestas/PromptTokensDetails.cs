48b2a92bec0a253bfa1cc5a64d7a37e159cdbff5
Cesar Fernando Carreño Peña <sql.programador16@opasascomco.onmicrosoft.com>
2025-01-28 15:29:43 -0500

    chose(PromptTokensDetails): dto para deserializar json de api

---
+++ entrenamientoAPI/Entities/Respuestas/PromptTokensDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace entrenamientoAPI.Entities.Respuestas
+{
+    public class PromptTokensDetails
+    {
+        public int cached_tokens { get; set; }
+        public int audio_tokens { get; set; }
+    }
+}
