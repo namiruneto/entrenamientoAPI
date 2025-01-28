c1bf4a583326150e41f2441672376387dbc1fda3
Cesar Fernando Carreño Peña <sql.programador16@opasascomco.onmicrosoft.com>
2025-01-28 15:28:13 -0500

    feat(choice): se mueve de ubicacion

--- entrenamientoAPI/Entities/Choice.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace entrenamientoAPI.Entities
-{
-    public class Choice
-    {
-        public Message message { get; set; }
-
-    }
-}
---
+++ entrenamientoAPI/Entities/Respuestas/Choice.cs
@@ -0,0 +1,11 @@
+﻿namespace entrenamientoAPI.Entities.Respuestas
+{
+    public class Choice
+    {
+        public int Index { get; set; }
+        public Message message { get; set; }
+        public object logprobs { get; set; } // Puede ser null o un objeto, cambiar según necesidad
+        public string finish_reason { get; set; }
+
+    }
+}
