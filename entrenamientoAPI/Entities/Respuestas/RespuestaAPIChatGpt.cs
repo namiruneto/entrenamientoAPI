e86e8c6a04f9259bc160804804b97ce5410f2e22
Cesar Fernando Carreño Peña <sql.programador16@opasascomco.onmicrosoft.com>
2025-01-28 15:29:58 -0500

    feat(RespuestaAPIChatGpt): respuesta api

---
+++ entrenamientoAPI/Entities/Respuestas/RespuestaAPIChatGpt.cs
@@ -0,0 +1,14 @@
+﻿namespace entrenamientoAPI.Entities.Respuestas
+{
+    public class RespuestaAPIChatGpt
+    {
+        public string id { get; set; }
+        public string Object { get; set; }
+        public long created { get; set; }
+        public string model { get; set; }
+        public List<Choice> choices { get; set; }
+        public Usage usage { get; set; }
+        public string service_tier { get; set; }
+        public string system_fingerprint { get; set; }
+    }
+}
