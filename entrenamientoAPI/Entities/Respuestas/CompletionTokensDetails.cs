a70c2af2db5dd22e9cdbaa8029e0a47736d902bd
Cesar Fernando Carreño Peña <sql.programador16@opasascomco.onmicrosoft.com>
2025-01-28 15:29:18 -0500

    feat(CompletiontOKENdETAILS): se agrega nuevo dto para saber los token utilizados

---
+++ entrenamientoAPI/Entities/Respuestas/CompletionTokensDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace entrenamientoAPI.Entities.Respuestas
+{
+    public class CompletionTokensDetails
+    {
+        public int reasoning_tokens { get; set; }
+        public int audio_tokens { get; set; }
+        public int accepted_prediction_tokens { get; set; }
+        public int rejected_prediction_tokens { get; set; }
+    }
+}
