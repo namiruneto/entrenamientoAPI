1d2ab1f38481b6898810e32b06f7cc9f40e85c61
Cesar Fernando Carreño Peña <sql.programador16@opasascomco.onmicrosoft.com>
2025-01-29 16:27:00 -0500

    feat(HistorialChat): tabla para almacenar todos los chat que se van generando

---
+++ BasesDatos/Tabla/HistorialChat.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[HistorialChat](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Pregunta] [varchar](max) NOT NULL,
+	[Respuesta] [varchar](max) NOT NULL,
+	[Asesor] [bit] NOT NULL,
+	[Categoria] [varchar](50) NOT NULL,
+	[SubCategoria] [varchar](50) NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
