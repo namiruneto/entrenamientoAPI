dbc35011238c1f3b6f281afdc6d64941487a912d
Cesar Fernando Carreño Peña <sql.programador16@opasascomco.onmicrosoft.com>
2025-01-29 16:22:38 -0500

    feat(HistorialChatType): modelo de datos para el formato de table que se va insertar utilizando el modelo de json

---
+++ entrenamientoAPI/Entities/BaseDatos/JsonParametros/HistorialChatType.cs
@@ -0,0 +1,15 @@
+﻿namespace entrenamientoAPI.Entities.BaseDatos.JsonParametros
+{
+    /// <summary>
+    /// Para consumir procedimiento almacenado 
+    /// </summary>
+    public class HistorialChatType : RespuestaDeApi
+    {
+        /// <summary>
+        /// Se convierte en un json para ser guardado en la base de datos
+        /// </summary>
+        public Guid IdChat { get; set; }
+        public string Pregunta { get; set; }
+        public bool Asesor { get; set; }
+    }
+}
