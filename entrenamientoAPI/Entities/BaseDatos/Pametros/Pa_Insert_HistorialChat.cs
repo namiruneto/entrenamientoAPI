b18f968599dcc17b9ebe0d366f0571f5abd8a33a
Cesar Fernando Carreño Peña <sql.programador16@opasascomco.onmicrosoft.com>
2025-01-29 16:10:46 -0500

    ci(Pa_Insert_HistorialChat): parametros de consumir el pa_insert_historialchat el cual utiliza un json

---
+++ entrenamientoAPI/Entities/BaseDatos/Pametros/Pa_Insert_HistorialChat.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace entrenamientoAPI.Entities.BaseDatos.Pametros
+{
+    /// <summary>
+    /// Para consumir procedimiento almacenado 
+    /// </summary>
+    public class Pa_Insert_HistorialChat 
+    {
+        /// <summary>
+        /// Se convierte en un json para ser guardado en la base de datos
+        /// </summary>
+        public string HistorialChat { get; set;}        
+    }
+}
