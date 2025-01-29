518f2817f1543597c964517bdf37c6e7f6979dea
Cesar Fernando Carreño Peña <sql.programador16@opasascomco.onmicrosoft.com>
2025-01-29 16:26:29 -0500

    feat(Pa_Insert_HistorialChat): procedimeibnto almacenado para guardar la informacion del caht que se estab ejecutando tanto asesor como ia

---
+++ BasesDatos/Procedimiento/PA_Insert_HistorialChat.sql
@@ -0,0 +1,21 @@
+﻿
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[PA_Insert_HistorialChat]
+    @HistorialChat NVARCHAR(MAX)
+AS
+BEGIN
+    INSERT INTO [dbo].[HistorialChat] (Id, Pregunta, Respuesta, Asesor, Categoria, SubCategoria)
+    SELECT 
+		JSON_VALUE(h.value, '$.IdChat') AS IdChat,
+        JSON_VALUE(h.value, '$.Pregunta') AS Pregunta,
+        ISNULL(JSON_VALUE(h.value, '$.respuesta'), '') AS Respuesta,
+        JSON_VALUE(h.value, '$.Asesor') AS Asesor,
+        ISNULL(JSON_VALUE(h.value, '$.categoria'), '') AS Categoria,
+        ISNULL(JSON_VALUE(h.value, '$.subcategoria'), '') AS SubCategoria
+    FROM OPENJSON(@HistorialChat) AS h;
+
+		select 'COMPLETADO' as Message, 1 as Estatus
+END
