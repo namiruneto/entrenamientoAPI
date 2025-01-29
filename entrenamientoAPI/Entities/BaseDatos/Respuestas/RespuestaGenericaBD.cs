f0815e0075d1058f5c69ea07ccdc24e22d401bc1
Cesar Fernando Carreño Peña <sql.programador16@opasascomco.onmicrosoft.com>
2025-01-29 16:11:35 -0500

    ci(RespuestaGenericaBD): respuesta generica despues de consumir PA

---
+++ entrenamientoAPI/Entities/BaseDatos/Respuestas/RespuestaGenericaBD.cs
@@ -0,0 +1,12 @@
+﻿namespace entrenamientoAPI.Entities.BaseDatos.Respuestas
+{
+    /// <summary>
+    /// Respuesta generica de la base de datos al consumir Procedimientos Almacenados
+    /// </summary>
+    public class RespuestaGenericaBD
+    {
+        public string Message { get; set; }
+        public bool Estatus { get; set; }
+
+    }
+}
